5d6c234a812b31a24c3b17198e06d9861deab6d6
NastyaKobtseva <svetka74@gmail.com>
2024-05-16 18:29:14 +0300

    finally-section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-// import './src/css/main.css'
-import './css/main.css'
+import './src/css/main.css'
+// import './css/main.css'
 import './src/css/modern-normalize.css'
 import './src/css/reset.css'
 import './src/js/script'
